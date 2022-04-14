0ca3fd51d06dcee33d62d5262ac348e093475107
juandiego9221 <juandiego9221@gmail.com>
2022-04-14 12:15:30 -0500

    fuente base

---
+++ PlatziWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PlatziWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
